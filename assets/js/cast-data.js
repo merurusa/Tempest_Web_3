d87da54d5430efdd8d9d6fdea00a6e84e505b7b5
yuki <87060282+merurusa@users.noreply.github.com>
2026-07-04 02:47:02 +0900

    Add files via upload

---
+++ assets/js/cast-data.js
@@ -0,0 +1,108 @@
+﻿window.TempestCastData = {
+  shops: {
+    various: { name: "Various", type: "Mix Bar", page: "various.html", castPage: "various-cast.html", placeholder: "../assets/cast/placeholders/various.png" },
+    solomon: { name: "Solomon", type: "Concept Cafe", page: "solomon.html", castPage: "solomon-cast.html", placeholder: "../assets/cast/placeholders/solomon.png" },
+    lively: { name: "Lively", type: "Poker Bar", page: "lively.html", castPage: "lively-cast.html", placeholder: "../assets/cast/placeholders/lively.png" },
+    charme: { name: "Charme", type: "ConceptCafe&Bar", page: "charme.html", castPage: "charme-cast.html", placeholder: "../assets/cast/placeholders/charme.png" },
+    strive: { name: "Strive", type: "Mix Bar", page: "strive.html", castPage: "strive-cast.html", placeholder: "../assets/cast/placeholders/strive.png" },
+    ebichanchi: { name: "えびちゃん家", type: "Gay Bar / Izakaya", page: "ebichanchi.html", castPage: "ebichanchi-cast.html", placeholder: "../assets/cast/placeholders/ebichanchi.png" }
+  },
+  casts: {
+    various: [
+      { id: "manager", role: "Manager", name: "ゆずは", image: "../assets/cast/various/manager.jpg", available: true },
+      { id: "cast01", role: "Cast", name: "かおり", image: "../assets/cast/various/cast01.jpg", available: true },
+      { id: "cast02", role: "Cast", name: "あいか", image: "../assets/cast/various/cast02.jpg", available: true },
+      { id: "cast03", role: "Cast", name: "もか", image: "../assets/cast/various/cast03.jpg", available: true },
+      { id: "cast04", role: "Cast", name: "かいり", image: "../assets/cast/various/cast04.jpg", available: true },
+      { id: "cast05", role: "Cast", name: "ゆう", image: "../assets/cast/various/cast05.jpg", available: true },
+      { id: "cast06", role: "Cast", name: "はるな", image: "../assets/cast/various/cast06.jpg", available: true },
+      { id: "cast07", role: "Cast", name: "はやて", image: "../assets/cast/various/cast07.jpg", available: true },
+      { id: "cast08", role: "Cast", name: "Comming soon", available: false }
+    ],
+    solomon: [
+      { id: "manager", role: "Manager", name: "りみ", image: "../assets/cast/solomon/manager.jpg", available: true },
+      { id: "leader", role: "Leader", name: "れもん", image: "../assets/cast/solomon/cast01.jpg", available: true },
+      { id: "cast01", role: "Cast", name: "あま", image: "../assets/cast/solomon/cast02.jpg", available: true },
+      { id: "cast02", role: "Cast", name: "れい", image: "../assets/cast/solomon/cast03.jpg", available: true },
+      { id: "cast03", role: "Cast", name: "しの", image: "../assets/cast/solomon/cast04.jpg", available: true },
+      { id: "cast04", role: "Cast", name: "Comming soon", available: false },
+      { id: "cast05", role: "Cast", name: "Comming soon", available: false },
+      { id: "cast06", role: "Cast", name: "Comming soon", available: false },
+      { id: "cast07", role: "Cast", name: "Comming soon", available: false },
+      { id: "cast08", role: "Cast", name: "Comming soon", available: false }
+    ],
+    lively: [
+      { id: "manager", role: "Manager", name: "そうた", image: "../assets/cast/lively/manager.jpg", available: true },
+      { id: "cast01", role: "Cast", name: "ふうた", image: "../assets/cast/lively/cast01.jpg", available: true },
+      { id: "cast02", role: "Cast", name: "ゆき", image: "../assets/cast/lively/cast02.jpg", available: true },
+      { id: "cast03", role: "Cast", name: "りお", image: "../assets/cast/lively/cast03.jpg", available: true },
+      { id: "cast04", role: "Cast", name: "かな", image: "../assets/cast/lively/cast04.jpg", available: true },
+      { id: "cast05", role: "Cast", name: "Comming soon", available: false },
+      { id: "cast06", role: "Cast", name: "Comming soon", available: false },
+      { id: "cast07", role: "Cast", name: "Comming soon", available: false },
+      { id: "cast08", role: "Cast", name: "Comming soon", available: false }
+    ],
+    charme: [
+      { id: "manager", role: "Manager", name: "りみ", image: "../assets/cast/charme/manager.jpg", available: true },
+      { id: "cast01", role: "Cast", name: "みるく", image: "../assets/cast/charme/cast01.jpg", available: true },
+      { id: "cast02", role: "Cast", name: "りく", image: "../assets/cast/charme/cast02.jpg", available: true },
+      { id: "cast03", role: "Cast", name: "さくら", image: "../assets/cast/charme/cast03.jpg", available: true },
+      { id: "cast04", role: "Cast", name: "まい", image: "../assets/cast/charme/cast04.jpg", available: true },
+      { id: "cast05", role: "Cast", name: "Comming soon", available: false },
+      { id: "cast06", role: "Cast", name: "Comming soon", available: false },
+      { id: "cast07", role: "Cast", name: "Comming soon", available: false },
+      { id: "cast08", role: "Cast", name: "Comming soon", available: false }
+    ],
+    strive: [
+      { id: "manager", role: "Manager", name: "あおい", image: "../assets/cast/strive/manager.jpg", available: true },
+      { id: "cast01", role: "Cast", name: "なつ", image: "../assets/cast/strive/cast01.jpg", available: true },
+      { id: "cast02", role: "Cast", name: "める", image: "../assets/cast/strive/cast02.jpg", available: true },
+      { id: "cast03", role: "Cast", name: "りず", image: "../assets/cast/strive/cast03.jpg", available: true },
+      { id: "cast04", role: "Cast", name: "すず", image: "../assets/cast/strive/cast04.jpg", available: true },
+      { id: "cast05", role: "Cast", name: "ちょも", image: "../assets/cast/strive/cast05.jpg", available: true },
+      { id: "cast06", role: "Cast", name: "るい", image: "../assets/cast/strive/cast06.jpg", available: true },
+      { id: "cast07", role: "Cast", name: "Comming soon", available: false },
+      { id: "cast08", role: "Cast", name: "Comming soon", available: false }
+    ],
+    ebichanchi: [
+      { id: "manager", role: "Manager", name: "Comming soon", available: false },
+      { id: "cast01", role: "Cast", name: "Comming soon", available: false },
+      { id: "cast02", role: "Cast", name: "Comming soon", available: false },
+      { id: "cast03", role: "Cast", name: "Comming soon", available: false },
+      { id: "cast04", role: "Cast", name: "Comming soon", available: false },
+      { id: "cast05", role: "Cast", name: "Comming soon", available: false },
+      { id: "cast06", role: "Cast", name: "Comming soon", available: false },
+      { id: "cast07", role: "Cast", name: "Comming soon", available: false },
+      { id: "cast08", role: "Cast", name: "Comming soon", available: false }
+    ]
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
